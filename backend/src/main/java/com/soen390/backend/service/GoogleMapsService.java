76554aee90303150cc7fc58ec657dd23fffbc425
robert-louis-lando <robertlouis_lando@outlook.com>
2026-01-31 12:56:57 -0500

    added-testing-for-googledirections-service

--- backend/src/main/java/com/soen390/backend/service/GoogleMapsService.java
+++ backend/src/main/java/com/soen390/backend/service/GoogleMapsService.java
@@ -1,14 +1,16 @@
 package com.soen390.backend.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.soen390.backend.dto.OutdoorDirectionResponse;
 import com.soen390.backend.dto.RouteStep;
 import com.soen390.backend.enums.ManeuverType;
 import com.soen390.backend.enums.TransportMode;
+import com.soen390.backend.exception.GoogleMapsDirectionsApiException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import tools.jackson.databind.JsonNode;
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +40,9 @@ public class GoogleMapsService {
         try {
             ObjectMapper mapper = new ObjectMapper();
             JsonNode root = mapper.readTree(json);
+            String status = root.path("status").asText();
+
+            checkResponseStatus(status);
 
             JsonNode route = root.path("routes").get(0);
             JsonNode leg = route.path("legs").get(0);
@@ -46,34 +51,60 @@ public class GoogleMapsService {
             String duration = leg.path("duration").path("text").asText();
             String polyline = route.path("overview_polyline").path("points").asText();
 
-            List<RouteStep> stepList = new ArrayList<>();
             JsonNode steps = leg.path("steps");
 
             JsonNode step1 = leg.path("steps").get(0);
             String modeOfTransport = step1.path("travel_mode").asText();
             TransportMode mode = TransportMode.valueOf(modeOfTransport.toLowerCase());
 
-            for (JsonNode step : steps) {
-                String instruction = step.path("html_instructions").asText();
-                String cleanInstruction = instruction.replaceAll("<[^>]*>", "");
+            return new OutdoorDirectionResponse(distance, duration, polyline, mode, processSteps(steps));
 
-                String stepDist = step.path("distance").path("text").asText();
-                String stepDur = step.path("duration").path("text").asText();
-                ManeuverType maneuverType;
-                if (!step.path("maneuver").isTextual()) {
-                    maneuverType = ManeuverType.STRAIGHT;
-                } else {
-                    String maneuver = step.path("maneuver").asText();
-                    maneuverType = ManeuverType.fromString(maneuver);
-                }
+        }
+        catch (GoogleMapsDirectionsApiException e) {
+            throw e;
+        }
+        catch (NullPointerException | JsonProcessingException e) {
+            throw new RuntimeException("Map data format error: The response from the map service was incomplete or unexpected.", e);
+        }
+        catch (Exception e) {
+            throw new RuntimeException("An unexpected error occurred while communicating with the Google Maps service.", e);
+        }
+    }
 
-                stepList.add(new RouteStep(cleanInstruction, stepDist, stepDur,maneuverType));
-            }
+    private List<RouteStep> processSteps(JsonNode steps){
 
-            return new OutdoorDirectionResponse(distance, duration, polyline, mode, stepList);
+        List<RouteStep> stepList = new ArrayList<>();
 
-        } catch (Exception e) {
-            throw new RuntimeException("Error parsing Google Directions response");
+        for (JsonNode step : steps) {
+
+            String instruction = step.path("html_instructions").asText();
+            String cleanInstruction = instruction.replaceAll("<[^>]*>", "");
+
+            String stepDist = step.path("distance").path("text").asText();
+            String stepDur = step.path("duration").path("text").asText();
+
+            ManeuverType maneuverType= handleMissingManeuver(step);
+
+            stepList.add(new RouteStep(cleanInstruction, stepDist, stepDur,maneuverType));
+        }
+        return stepList;
+
+    }
+
+    private ManeuverType handleMissingManeuver(JsonNode step){
+        ManeuverType maneuverType;
+        if (!step.path("maneuver").isTextual()) {
+            maneuverType = ManeuverType.STRAIGHT;
+        } else {
+            String maneuver = step.path("maneuver").asText();
+            maneuverType = ManeuverType.fromString(maneuver);
+        }
+        return maneuverType;
+    }
+
+    private void checkResponseStatus(String status){
+        if(!status.equals("OK")){
+            throw new GoogleMapsDirectionsApiException("Directions not found. Please check your start and end locations.");
         }
     }
 
